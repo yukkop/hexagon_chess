4065a198d7bdf97669d5c023364be9fadba4045d
Lysmi <lysmi.mx@mail.ru>
2022-11-24 02:09:40 +0100

    feat: pown attack added

--- lib/game/figures/pawn.dart
+++ lib/game/figures/pawn.dart
@@ -2,6 +2,7 @@ import 'package:flame/components.dart';
 import 'package:hexagon_chess/core/constants.dart';
 import 'package:hexagon_chess/game/assets_store.dart';
 import 'package:hexagon_chess/game/figures/figure.dart';
+import 'package:hexagon_chess/game/figures/none_figure.dart';
 
 import '../board.dart';
 import '../cell.dart';
@@ -30,31 +31,60 @@ class Pawn extends Figure {
     List<Cell> result = [];
     var position = cell.positionOnTheBoard;
     int targetRow = 0;
+    int frontAttackRow = 0;
     bool inHome = false; //pawn in his own half field
     if (color == GameColor.white) {
       targetRow = position.y.toInt() + 1;
+      frontAttackRow = targetRow + 1;
       if (position.y < 6) {
         inHome = true;
       }
     } else {
       targetRow = position.y.toInt() - 1;
+      frontAttackRow = targetRow - 1;
       if (position.y > 6) {
         inHome = true;
       }
     }
     if (targetRow > -1 && targetRow < 13) {
-      if (inHome) {
-        result.addAll([
-          board.cells[targetRow][cell.positionOnTheBoard.x.toInt()],
-          board.cells[targetRow][cell.positionOnTheBoard.x.toInt() + 1],
-        ]);
-      } else {
-        result.add(board.cells[targetRow][cell.positionOnTheBoard.x.toInt()]);
-        if (cell.positionOnTheBoard.x != 0) {
-          result.add(
-              board.cells[targetRow][cell.positionOnTheBoard.x.toInt() - 1]);
+      List<Cell> moveArray = [];
+      if (cell.positionOnTheBoard.x > -1 &&
+          cell.positionOnTheBoard.x < board.cells[targetRow].length) {
+        moveArray
+            .add(board.cells[targetRow][cell.positionOnTheBoard.x.toInt()]);
+      }
+      if (cell.positionOnTheBoard.x + (inHome ? 1 : -1) > -1 &&
+          cell.positionOnTheBoard.x + (inHome ? 1 : -1) <
+              board.cells[targetRow].length + 1) {
+        moveArray.add(board.cells[targetRow]
+            [cell.positionOnTheBoard.x.toInt() + (inHome ? 1 : -1)]);
+      }
+      List<Cell> attackArray = [];
+      if (frontAttackRow > -1 && frontAttackRow < 13) {
+        if ((cell.positionOnTheBoard.y.toInt() == 5 ||
+                cell.positionOnTheBoard.y.toInt() == 7) &&
+            inHome) {
+          attackArray.add(
+              board.cells[frontAttackRow][cell.positionOnTheBoard.x.toInt()]);
+        } else {
+          if (cell.positionOnTheBoard.y.toInt() != 6 && inHome) {
+            attackArray.add(board.cells[frontAttackRow]
+                [cell.positionOnTheBoard.x.toInt() + (inHome ? 1 : -1)]);
+          }
         }
       }
+      if (cell.positionOnTheBoard.x.toInt() - (inHome ? 1 : 2) > -1) {
+        attackArray.add(board.cells[targetRow]
+            [cell.positionOnTheBoard.x.toInt() - (inHome ? 1 : 2)]);
+      }
+      if (cell.positionOnTheBoard.x.toInt() + (inHome ? 2 : 1) <
+          board.cells[targetRow].length) {
+        attackArray.add(board.cells[targetRow]
+            [cell.positionOnTheBoard.x.toInt() + (inHome ? 2 : 1)]);
+      }
+      result.addAll(moveArray.where((element) => element.figure is NoneFigure));
+      result.addAll(attackArray.where((element) =>
+          element.figure is! NoneFigure && element.figure.color != color));
     }
 
     return result;
